7c9f33b77225ab788969553b694c04b549875dbe
kantilal1 <41815073+kantilal1@users.noreply.github.com>
2019-01-29 22:40:58 +0530

    smoke test

--- src/test/java/com/farm/qa/testcases/FarmAppTest.java
+++ src/test/java/com/farm/qa/testcases/FarmAppTest.java
@@ -73,27 +73,82 @@ public class FarmAppTest extends TestBase {
 
 	}
 
-	@Test(priority = 1)
+	
+	
+/*	public void test_all_tabs(){		
+		String home_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='Home']")).getText();
+		String mandi_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='Mandi']")).getText();
+		String agronomy_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='Agronomy']")).getText();
+		String chat_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='Chat']")).getText();
+		String more_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='More']")).getText();  
+		String tabs[] = {"home_btn", "mandi_btn", "agronomy_btn", "chat_btn", "more_btn"};		
+		for (int i=0; i<tabs.length; i++){			
+			if (tabs[i].equalsIgnoreCase("Home")){
+				driver.findElement(By.xpath("//android.widget.TextView[@text='Home']")).click();
+				String home_page_title = driver.findElement(By.xpath("//android.widget.TextView[@text='Home']")).getText();
+				Assert.assertEquals(home_page_title, "Home", "Home page window is not displayed upon clicking on Home button");								
+			}	
+			else if(tabs[i].equalsIgnoreCase("mandi_btn")){
+				 driver.findElement(By.xpath("//android.widget.TextView[@text='Mandi']")).click();
+				 String mandi_page_title = driver.findElement(By.xpath("//android.widget.TextView[@text='Mandi Prices']")).getText();
+					Assert.assertEquals(mandi_page_title, "Mandi Prices", "Home page window is not displayed upon clicking on Home button");								
+			}
+			
+			else if(tabs[i].equalsIgnoreCase("agronomy_btn")){
+				
+				driver.findElement(By.xpath("//android.widget.TextView[@text='Agrorany']")).click();
+				String agrorany_page_title = driver.findElement(By.xpath("//android.widget.TextView[@text='Agrorany']")).getText();
+				Assert.assertEquals(agrorany_page_title, "Agrorany", "Home page window is not displayed upon clicking on Home button");								
+
+						}
+						
+			else if(tabs[i].equalsIgnoreCase("chat_btn")){
+				
+				driver.findElement(By.xpath("//android.widget.TextView[@text='Chat']")).click();
+				String chat_page_title = driver.findElement(By.xpath("//android.widget.TextView[@text='Chat']")).getText();
+				Assert.assertEquals(chat_page_title, "Chat", "Home page window is not displayed upon clicking on Home button");								
+
+				
+			}
+			else if(tabs[i].equalsIgnoreCase("more_btn")){
+				driver.findElement(By.xpath("//android.widget.TextView[@text='More']")).click();
+				String more_page_title = driver.findElement(By.xpath("//android.widget.TextView[@text='More']")).getText();
+				Assert.assertEquals(more_page_title, "Home", "Home page window is not displayed upon clicking on Home button");								
+
+				
+			}
+			
+			
+		}
+		
+		//String more_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='Mandi Prices']")).getText();
+		//String more_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='Chat']")).getText();
+		//String more_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='More']")).getText();
+
+		
+	}
+*/	
+	/*@Test(priority = 1)
 	public void goverment_Scheme() {
 
 		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 		WebElement more_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='More']"));
 		more_btn.click();
-		String more_page_title = more_btn.getText();
-		System.out.println(more_page_title);
+		String more_page_title =  driver.findElement(By.xpath("//android.widget.TextView[@text='More']")).getText();
+		System.out.println("more_page_title==>" +more_page_title);
 		Assert.assertEquals(more_page_title, "More", "More tab window is not displayed upon clicking on More button");
 		WebElement governmentschemes_btn = driver
 				.findElement(By.xpath("//android.widget.TextView[@text='Government Schemes']"));
 		governmentschemes_btn.click();
 		String title_of_page = driver.findElement(By.id("com.climate.farmrise:id/toolBar_Title")).getText();
-		System.out.println(title_of_page);
+		System.out.println("title_of_page" +title_of_page);
 		Assert.assertEquals(title_of_page, "Government Schemes",
 				"Government Schemes window is not displayed upon clicking on Government Schemes button");
 		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 		String sorry_txt = driver.findElement(By.id("com.climate.farmrise:id/sorry_txt")).getText();
-		System.out.println(sorry_txt);
+		System.out.println("sorry_txt==>" +sorry_txt);
 		String no_scheme_msg = driver.findElement(By.id("com.climate.farmrise:id/no_content_desc")).getText();
-		System.out.println(no_scheme_msg);
+		System.out.println("no_scheme_msg==>"+no_scheme_msg);
 		if (sorry_txt.equalsIgnoreCase("We're sorry!") && no_scheme_msg.equalsIgnoreCase(
 				"We regret to inform you that Government Schemes are unavailable in your region. Watch this space for updates.")) {
 			System.out.println("No schemes in your region");
@@ -102,8 +157,9 @@ public class FarmAppTest extends TestBase {
 		}
 
 	}
-
-	@Test(priority = 2)
+*/
+	
+/*	@Test(priority = 2)
 	public void chat_test() {
 
 		WebElement chat_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='Chat']"));
@@ -170,36 +226,28 @@ public class FarmAppTest extends TestBase {
 		String wrong_otp_popup = driver.findElement(By.id("com.climate.farmrise:id/dialog_title")).getText();
 		Assert.assertEquals(wrong_otp_popup, "Wrong OTP",
 				"Wrong OTP popup is not displayed upon entering invalid OTP ");
-
-		/*
-		 * if (wrong_otp_popup.equalsIgnoreCase("Wrong OTP")) {
-		 * 
-		 * System.out.println("OTP popup is displayed");
-		 * driver.findElement(By.id("right_button")).click(); } else {
-		 * 
-		 * System.out.println("OTP popup is not displayed"); }
-		 */
-
+		driver.findElement(By.id("right_button")).click();
 		driver.findElement(By.id("btn_back")).click();
 		driver.pressKeyCode(AndroidKeyCode.BACK);
 		System.out.println("code ended");
 
-	}
+	}*/
 
 	@Test(priority = 3)
 	public void settings_tab() {
+		
 		WebElement more_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='More']"));
 		more_btn.click();
 		WebElement settings_btn = driver.findElement(By.xpath("//android.widget.TextView[@text='Settings']"));
 		settings_btn.click();
-		String setting_page_title = settings_btn.getText();
-		System.out.println(setting_page_title);
+		String setting_page_title = driver.findElement(By.xpath("//android.widget.TextView[@text='Settings']")).getText();
+		System.out.println("setting_page_title==>" +setting_page_title);
 		Assert.assertEquals(setting_page_title, "Settings",
 				"Settings window is not displayed upon clicking on Settings button");
 		WebElement notification_btn = driver.findElement(By.id("notifyPreference"));
 		notification_btn.click();
-		String notification_page_tiitle = notification_btn.getText();
-		System.out.println(notification_page_tiitle);
+		String notification_page_tiitle = driver.findElement(By.xpath("//android.widget.TextView[@text='Notification Preferences']")).getText();
+		System.out.println("notification_page_tiitle==>" +notification_page_tiitle);
 		Assert.assertEquals(notification_page_tiitle, "Notification Preferences",
 				"Notification Preferences window is not displayed upon clicking on Notification Preferences button");
 		WebElement switchNotification_radio_btn = driver.findElement(By.id("switchNotification"));
