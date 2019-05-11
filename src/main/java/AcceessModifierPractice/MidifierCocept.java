cffc1545752869af7612bf9b25c32d55469ea2e2
Arjun Thawkar <arjun@torenzo.com>
2019-05-11 17:56:51 +0530

    m5

---
+++ src/main/java/AcceessModifierPractice/MidifierCocept.java
@@ -0,0 +1,51 @@
+package AcceessModifierPractice;
+
+public class MidifierCocept {
+
+	
+	
+	private static String firstname = "";
+	
+	public void setValue(){
+		this.firstname = "Bhup";
+		
+	}
+	
+	public static String getValue(){
+		return firstname;
+		
+	}
+	
+
+	static String middleName = "Pralhad";
+
+	protected static String lastName = "Patil";
+
+	public  static String villageName = "UK";
+
+	private static void rollNum() {
+
+		System.out.println("rollNum()");
+	}
+
+	static void  regNum() {
+
+		System.out.println("regNum()");
+	}
+	
+	protected static void collageName() {
+
+		System.out.println("collageName()");
+	}
+	
+	public static void district() {
+
+		System.out.println("district()");
+	}
+	
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/main/java/StringConcept/StringConcept.java
+++ src/main/java/StringConcept/StringConcept.java
@@ -1,38 +1,85 @@
 package StringConcept;
 
-public class StringConcept {
+import AcceessModifierPractice.MidifierCocept;
+import static AcceessModifierPractice.MidifierCocept.*;
+
+public class StringConcept extends MidifierCocept {
+
+	static {
+		System.out.println("Hello sachin");
+
+	}
 
 	public static void main(String[] args) {
-	
-		String firstName  = "Sachin";
-		String middleName  = "Pralhad";
-		String lastName  = "Patil";
-		
+
+		MidifierCocept mc = new MidifierCocept();
+
+		district();
+		collageName();
+		mc.setValue();
+		String firstName = "Sachin";
+		String middleName = "Pralhad";
+		String lastName = "Patil";
+
 		String str = lastName.concat(firstName);
-		
-		System.out.println("lastName.concat(firstName);" +str);
-				
-		System.out.println(firstName +" "+lastName );
-		
+
+		System.out.println("lastName.concat(firstName);" + getValue());
+
+		System.out.println(firstName + " " + lastName);
+
 		firstName.length();
-		
-		System.out.println("firstName.length();" +	firstName.length());
-		
-		System.out.println("firstName.charAt(3);" +	firstName.charAt(3));
-		
-		
-		lastName=firstName;
-		System.out.println("lastName.equals(firstName);" +	lastName.equals(firstName));
-		
-		System.out.println("lastName.equalsIgnoreCase(firstName)" +	lastName.equalsIgnoreCase(firstName));
-		
-		
-		
-		System.out.println("firstName.indexOf(4)" +	firstName.indexOf('a'));
-		
-		
-		System.out.println("firstName.substring(0)" +	firstName.substring(3));
-		
+
+		System.out.println("firstName.length();" + firstName.length());
+
+		System.out.println("firstName.charAt(3);" + firstName.charAt(3));
+
+		lastName = firstName;
+		System.out.println("lastName.equals(firstName);" + lastName.equals(firstName));
+
+		System.out.println("lastName.equalsIgnoreCase(firstName)" + lastName.equalsIgnoreCase(firstName));
+
+		System.out.println("firstName.indexOf(4)" + firstName.indexOf('a'));
+
+		System.out.println("firstName.substring(0)" + firstName.substring(3));
+
+		System.out.println("firstName.substring(0)" + firstName.substring(3));
+
+		System.out.println("firstName.substring(0)" + firstName.substring(2, 4));
+
+		String friendName = "      Arjun    ";
+
+		System.out.println("friendName.substring(0)" + friendName.trim());
+
+		String tools = "Selenium-QTP-Appium";
+
+		System.out.println("friendName.charAt(3)" + friendName.charAt(3));
+
+		String toolsName[] = tools.split("-");
+		System.out.println("toolsName" + toolsName);
+
+		for (String str1 : toolsName) {
+			System.out.println("str1" + str1);
+
+		}
+
+		String s1 = "Rahit";
+		String s2 = "Kapam";
+
+		System.out.println("s1" + s1);
+		System.out.println("s2" + s2);
+		s2 = "patil";
+		System.out.println("s2" + s2);
+		System.out.println("s1" + s1);
+
+		String rain = "The rain has started here sachin";
+
+		System.out.println("rain length" + rain.length());
+
+		System.out.println("rain length=>" + rain.indexOf("s"));
+
+		System.out.println("rain length=>" + rain.indexOf("s", rain.indexOf("s") + 1));
+		System.out.println("rain length=>" + rain.indexOf("s", rain.indexOf("s") + 1 + 2));
+
 	}
 
 }
